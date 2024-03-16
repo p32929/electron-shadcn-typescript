d999f4d94df7bee93939b2f9c5293b889698abde
p32929 <p32929@yahoo.com>
2024-03-17 01:28:22 +0400

    added small counter

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,61 +1,40 @@
-import * as React from "react"
 
 import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { useState } from "react"
 
 function App(): JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const [counter, setCounter] = useState(0)
+
+  const increase = () => {
+    setCounter(counter + 1)
+  }
+
+  const decrease = () => {
+    setCounter(counter - 1)
+  }
 
   return (
     <div className="flex flex-row items-center justify-center p-16">
       <Card className="w-[350px]">
         <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
+          <CardTitle>Counter: {counter}</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="Name of your project" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="framework">Framework</Label>
-                <Select>
-                  <SelectTrigger id="framework">
-                    <SelectValue placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectItem value="next">Next.js</SelectItem>
-                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                    <SelectItem value="astro">Astro</SelectItem>
-                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </div>
-          </form>
+          <p>
+            Click on increase / decrease to check the state changes
+          </p>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
+          <Button onClick={decrease} variant="outline">Decrease --</Button>
+          <Button onClick={increase}>Increase ++</Button>
         </CardFooter>
       </Card>
     </div>
