f38c3b26f122fc84815a027015f1295249999f48
p32929 <p32929@yahoo.com>
2024-03-17 11:47:26 +0400

    fixed dev script

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,8 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 600,
+    height: 400,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
